45e727d0845e0d4f1b1c28626611002c00e96a2f
Marcus Reis <mvsantosreis@gmail.com>
2024-08-27 20:31:22 +0100

    Retrieving essential data from API

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,5 +1,43 @@
-<script setup></script>
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
 
 <template>
-  <h1 class="p-5">Hello World</h1>
+  <div class="bg-white relative border rounded-lg">
+    <div class="w-full text-sm text-left text-gray-500">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th class="px-4 py-3">InternalClient</th>
+          <th class="px-4 py-3">Client</th>
+          <th class="px-4 py-3">Module</th>
+          <th class="px-4 py-3">Language</th>
+          <th class="px-4 py-3">Mobile</th>
+          <th class="px-4 py-3">Date</th>
+          <th class="px-4 py-3">
+            <span class="sr-only">Actions</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td class="px4 py3 font-medium text-gray-900">
+            {{ item.internal_client }}
+          </td>
+          <td class="px4 py3 font-medium text-gray-900">{{ item.client }}</td>
+          <td class="px4 py3">{{ item.module }}</td>
+          <td class="px4 py3">{{ item.language }}</td>
+          <td class="px4 py3">{{ item.mobile }}</td>
+          <td class="px4 py3">
+            {{ item.created_at }}
+          </td>
+          <td class="px-4 py-3 flex item-center justify-end">Details</td>
+        </tr>
+      </tbody>
+    </div>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import DataTable from "../components/DataTable.vue";
+import DataTable from '../components/DataTable.vue'
+import { onMounted, ref } from 'vue'
+
+const items = ref([])
+const API_URL = import.meta.env.VITE_API_URL
+
+onMounted(async () => {
+  const response = await fetch(API_URL + `/api/trackinginfo/`)
+  items.value = await response.json()
+})
 </script>
 
 <template>
   <main>
-    <DataTable />
+    <div class="p-8 bg-gray-100 min-h-screen">
+      <DataTable :items="items.data" />
+    </div>
   </main>
 </template>
